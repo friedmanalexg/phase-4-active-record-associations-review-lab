285c61cab460981174f438165a7985582c7099dc
Alex <friedmanalexg@gmail.com>
2022-09-01 16:34:21 -0400

    commit tree 6d3a0bfbbf6e902455b7d2749ff254d0dd89216f

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+4.times do Passenger.create()
+4.times do Taxi.create()
+Ride.create(passenger_id:1 , taxi_id:4 )    
+Ride.create(passenger_id:2 , taxi_id:3 )    
+Ride.create(passenger_id:3 , taxi_id:2 )    
+Ride.create(passenger_id:4 , taxi_id:1 )    
+Ride.create(passenger_id:4 , taxi_id:4 )    
+Ride.create(passenger_id:2 , taxi_id:3 )    
+Ride.create(passenger_id:1 , taxi_id:2 )    
+Ride.create(passenger_id:3 , taxi_id:1 )    
